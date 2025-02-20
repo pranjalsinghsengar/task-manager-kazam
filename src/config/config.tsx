b2250a81232388d07fcf00c40ea9fbfb2a9e02aa
Pranjal Singh Sengar <pranjalsinghsengar94@gmail.com>
2025-02-20 21:00:11 +0530

    init

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,2 +1,2 @@
-// export const API_URL = "https://taskmanager-server-pm9v.onrender.com";
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://taskmanager-server-pm9v.onrender.com";
+// export const API_URL = "http://localhost:3000";
